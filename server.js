a81041a3c4416d0bb24d37ef609d52e82026a3bc
Samim Kohistani <samimkohistani@samims-air.myfiosgateway.com>
2022-10-04 13:55:28 -0400

    MogoDB/Mongoose connected

--- server.js
+++ server.js
@@ -1,20 +1,51 @@
 // DEPENDENCIES
+// get .env variables
+require("dotenv").config();
+// pull PORT from .env, give default value of 4949
+// pull MONGODB_URL from .env
+const { PORT = 4949, MONGODB_URL } = process.env;
+// import express
 const express = require('express');
-const path = require('path');
-const routes = require('./routes');
-const db = require('./config/connection');
-
+// create application object
 const app = express();
-const PORT = process.env.PORT || 3001
+// import mongoose
+const mongoose = require("mongoose");
+// import path
+const path = require('path');
+// import routes
+const routes = require('./routes');
+// import middleWare
+const cors = require("cors");
+const morgan = require("morgan");
+
+// DATABASE CONNECTION
+////////////////////////////////
+// Establish Connection
+mongoose.connect(MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+
+
+
+  // Connection Events
+mongoose.connection
+.on("open", () => console.log("You are connected to mongoose"))
+.on("close", () => console.log("You are disconnected from mongoose"))
+.on("error", (error) => console.log(error));
+
+
+
 // MiddleWare
 ////////////////////////////////
 app.use(cors()); // to prevent cors errors, open access to all origins
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev")); // logging
 app.use(express.json()); // parse json bodies
-
-
-
 app.use(routes);
 
-app.listen(PORT, () => console.log("Listening on localhost " + PORT));
 
+
+  // LISTENER
+////////////////////////////////
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
