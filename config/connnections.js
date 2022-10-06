96156dd0ef5dd8c2288b6f437fe87b63b2398547
Zachary Gexler <110911839+zachgexler@users.noreply.github.com>
2022-10-06 07:05:28 -0400

    added config folder and connection.js file for homebase

---
+++ config/connnections.js
@@ -0,0 +1,10 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect(MONGODB_URL, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//   });
+
+
+// module.exports = mongoose.connection
+    
